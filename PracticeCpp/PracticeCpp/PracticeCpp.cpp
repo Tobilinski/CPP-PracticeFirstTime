0df25036c556ef35d01a5f0ef119d424823c9439
Tobias Noelkensmeier <Tobiassherco125@gmail.com>
2023-12-20 15:11:44 +0200

    Completed a Banking system Project in Console

--- PracticeCpp/PracticeCpp/PracticeCpp.cpp
+++ PracticeCpp/PracticeCpp/PracticeCpp.cpp
@@ -8,23 +8,73 @@ using namespace std;
 //void FizzBuzz();
 //void Pointers();
 
+
+void showBalance(double Balance);
+double Withdraw(double Balance);
+double Deposit();
+
 //int num = 11;
 int main()
 {
-    int num = 0;
-    string Greeting = "Hi";
-    cin >> num;
+    int Choice = 0;
+    double Balance = 0;
+    do
+    {
+        cout << "************* Banking System *************\n";
+        cout << "Make a choice between 4 differnet input\n";
+        cout << "1. Show Balance\n";
+        cout << "2. Deposit\n";
+        cout << "3. Withdraw\n";
+        cout << "4. Exit\n";
+        cin >> Choice;
 
-    if (num == 10) {
-        cout << Greeting;
-    }
-    
+        switch (Choice) {
+        case 1:
+            showBalance(Balance);
+            break;
+        case 2:
+            Balance += Deposit();
+            break;
+        case 3:
+            Balance -= Withdraw(Balance);
+            break;
+        case 4:
+            cout << "Have a nice day sir !!\n"
+            break;
+        default:
+            break;
+        }
+        cout << "************* *************** ************\n";
+    } while (Choice != 4);
+
     //Fibin();
     //FizzBuzz();
     //Pointers();
     return 0;
 }
 
+void showBalance(double Balance) {
+    cout << "Your current Balance is: " << Balance << endl;
+}
+double Deposit() {
+    double amount = 0;
+    cout << "Make your deposit of how ever much you want: ";
+    cin >> amount;
+
+    return amount;
+}
+double Withdraw(double Balance) {
+    double amount = 0;
+    cout << "Set your Withdraw of how ever much you want: ";
+    cin >> amount;
+    if (Balance - amount > 0) {
+        return amount;
+    }
+    else {
+        cout << "You are trying to withdraw more than you have";
+        return 0;
+    }
+}
 /*void Pointers() {
     int* ptr = &num;
     cout << *ptr << endl;
